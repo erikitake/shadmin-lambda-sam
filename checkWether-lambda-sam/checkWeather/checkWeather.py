402db1871b98a60b244bc26e267a5108b4a65825
erikitake <e.rikitake@icloud.com>
2021-08-07 12:27:42 +0000

    for wind_gust

--- checkWether-lambda-sam/checkWeather/checkWeather.py
+++ checkWether-lambda-sam/checkWeather/checkWeather.py
@@ -19,12 +19,22 @@ def lambda_handler(event, context):
     url = BASEURL + PARAM + APIKEY
     r = requests.get(url).json()
 
-    strWeather ={"temp": str(r["current"]["temp"]), "humidity": str(r["current"]["humidity"]), \
-      "time": str(r["current"]["dt"]), "feels_like": str(r["current"]["feels_like"]), \
-      "pressure": str(r["current"]["pressure"]), "dew_point": str(r["current"]["dew_point"]), \
-      "uvi": str(r["current"]["uvi"]), "wind_speed": str(r["current"]["wind_speed"]), \
-      "wind_deg": str(r["current"]["wind_deg"]), "wind_gust": str(r["current"]["wind_gust"]), \
-      "weather": r["current"]["weather"][0]["description"] }
+    logger.info('Event: %s', str(r))
+    
+    if('wind_gust' not in r) :
+      strWeather ={"temp": str(r["current"]["temp"]), "humidity": str(r["current"]["humidity"]), \
+        "time": str(r["current"]["dt"]), "feels_like": str(r["current"]["feels_like"]), \
+        "pressure": str(r["current"]["pressure"]), "dew_point": str(r["current"]["dew_point"]), \
+        "uvi": str(r["current"]["uvi"]), "wind_speed": str(r["current"]["wind_speed"]), \
+        "wind_deg": str(r["current"]["wind_deg"]), \
+        "weather": r["current"]["weather"][0]["description"] }
+    elif('wind_gust' in r) :
+      strWeather ={"temp": str(r["current"]["temp"]), "humidity": str(r["current"]["humidity"]), \
+        "time": str(r["current"]["dt"]), "feels_like": str(r["current"]["feels_like"]), \
+        "pressure": str(r["current"]["pressure"]), "dew_point": str(r["current"]["dew_point"]), \
+        "uvi": str(r["current"]["uvi"]), "wind_speed": str(r["current"]["wind_speed"]), \
+        "wind_deg": str(r["current"]["wind_deg"]), "wind_gust": str(r["current"]["wind_gust"]), \
+        "weather": r["current"]["weather"][0]["description"] }
 
     return strWeather
     
