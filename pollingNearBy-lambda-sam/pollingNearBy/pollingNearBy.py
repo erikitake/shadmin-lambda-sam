871dd6dacc8e2b78229ad20d8e47dd20ea28b661
erikitake <e.rikitake@icloud.com>
2021-08-09 07:00:50 +0000

    amend

--- pollingNearBy-lambda-sam/pollingNearBy/pollingNearBy.py
+++ pollingNearBy-lambda-sam/pollingNearBy/pollingNearBy.py
@@ -14,40 +14,33 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
     logger.info('Event: %s', event)
 
-    #connect
     db_connect = os.getenv('db_connect')
     logger.info('json db_connect %s', db_connect)
-
     conn = psycopg2.connect(db_connect)
     logger.info('connected')
-    #Open a cursor to perform database operations
     cur = conn.cursor()
 
+    # locationテーブルを更新している対象を取得し、その分処理を繰り返し
     sqlstring = "select updated_by from locations group by updated_by;"
     logger.info('sqlstring: %s', sqlstring)
     cur.execute(sqlstring)
     rows = cur.fetchall()
     logger.info('rows: %s', rows)
+
     for index, item in enumerate(rows):
+        print("start : ", str(item))
         execProcess(event, context, item[0])
 
-    #Make the changes to the database persistent
-    conn.commit()
-    # Close communication with the database
     cur.close()
     conn.close()
     logger.info('conn close.')
 
     return
     
+# 位置情報が範囲内にあるかを判定する。in or out. 判定情報をDBに登録する。
 def execProcess(event, context, item):
-    #connect
     db_connect = os.getenv('db_connect')
-    logger.info('json db_connect %s', db_connect)
-
     conn = psycopg2.connect(db_connect)
-    logger.info('connected')
-    #Open a cursor to perform database operations
     cur = conn.cursor()
 
     sqlstring = "select * from locations where updated_by='" + item + "' order by updated_at desc limit 1;" 
@@ -56,6 +49,7 @@ def execProcess(event, context, item):
     row = cur.fetchone()
     logger.info('row: %s', row)
 
+    locationAt = os.getenv('locationAt')
     checkRange = []
     checkRange.append(os.getenv('luLati'))
     checkRange.append(os.getenv('luLong'))
@@ -65,7 +59,7 @@ def execProcess(event, context, item):
     checkRange.append(os.getenv('roLong'))
     checkRange.append(os.getenv('ruLati'))
     checkRange.append(os.getenv('ruLong'))
-    print(checkRange)
+    print("checkRange : ", checkRange)
 
     point = Feature(geometry=Point((float(row[2]), float(row[3]))))
     polygon = Polygon([[
@@ -81,13 +75,12 @@ def execProcess(event, context, item):
     else:
         inOut="out"
 
-    sqlstring = "INSERT INTO location_status (lid, location_status, created_at, created_by, updated_at, updated_by) values ('" \
-     + str(row[0]) + "', '" + str(inOut) + "', CURRENT_TIMESTAMP, '" + str(item) + "', CURRENT_TIMESTAMP, '" + str(item) + "');" 
+    sqlstring = "INSERT INTO location_status (lid, location_status, location_at, created_at, created_by, updated_at, updated_by) values ('" \
+     + str(row[0]) + "', '" + str(inOut) + "', '" + str(locationAt) + "', CURRENT_TIMESTAMP, '" + str(item) + "', CURRENT_TIMESTAMP, '" + str(item) + "');" 
     logger.info('sqlstring: %s', sqlstring)
     cur.execute(sqlstring)
-
-    #Make the changes to the database persistent
     conn.commit()
+
     # Close communication with the database
     cur.close()
     conn.close()
