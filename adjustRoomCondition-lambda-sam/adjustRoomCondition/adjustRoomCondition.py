e952ed50e49ab53f01d6a8112112f594ec55aae1
erikitake <e.rikitake@icloud.com>
2021-08-21 15:45:56 +0000

    amend

---
+++ adjustRoomCondition-lambda-sam/adjustRoomCondition/adjustRoomCondition.py
@@ -0,0 +1,68 @@
+import json
+import logging
+import psycopg2
+import os
+import shutil
+from datetime import datetime, timedelta, timezone
+import boto3
+import requests
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+ssm = boto3.client('ssm')
+
+def lambda_handler(event, context):
+    logger.info('Event: %s', event)
+
+    if event.get('season',None) == "":
+        print('none')
+        return {}
+    
+    season = event['season']
+    status = event['status']
+    roomName = event['roomName']
+    checkDegree = event['checkDegree']
+    degree = event['degree']
+
+    nr_auth = ssm.get_parameter(Name='nr_auth', WithDecryption=True)['Parameter']['Value']
+    print('nr_auth: ', nr_auth)
+    nr_ac = ssm.get_parameter(Name='nr_ac', WithDecryption=True)['Parameter']['Value']
+    url = "https://api.nature.global/1/appliances/" + nr_ac + "/aircon_settings"
+    print('url: ', url)
+
+    opmode = ""
+    avol = ""
+    adjustDegree = ""
+    if season == "summer":
+        opmode = "cool"
+        if status == "hot":
+            avol = "5"
+            adjustDegree = "18"
+        elif status == "cold":
+            avol = "auto"
+            adjustDegree = "26"
+    elif season == "winter":
+        opmode = "hot"
+        if status == "cold":
+            avol = "5"
+            adjustDegree = "28"
+        elif status == "hot":
+            avol = "auto"
+            adjustDegree = "22"
+
+    param = "operation_mode="+opmode+"&air_volume="+avol+"&temperature="+adjustDegree
+    print("param :", param)
+
+    headers = {"accept":"application/json", "Content-Type":"application/x-www-form-urlencoded", "Authorization":nr_auth}
+    r = requests.post(url, headers=headers, params=param)
+
+    print("r status:", r.status_code)
+    print("r :", r.json())
+
+    # notify private line
+    honbunStr = roomName + "の今の部屋の温度が" + degree + "度だから、" + opmode + " " + avol + " " + adjustDegree + "度 にエアコンを設定しました"
+    mainStr = {"key0": {"flg": 0, "mainStr": honbunStr}}
+
+    return mainStr
+    
--- pollingRoomCondition-lambda-sam/pollingRoomCondition/pollingRoomCondition.py
+++ pollingRoomCondition-lambda-sam/pollingRoomCondition/pollingRoomCondition.py
@@ -23,10 +23,27 @@ def lambda_handler(event, context):
     dt_now = datetime.now()
     nowMonth = dt_now.strftime('%m')
 
+    users = os.getenv('users')
+    users = users.split(":")
+    print("users : ", users)
+    userStr = ""
+    for index, user in enumerate(users):
+        # 家にいるときのみ実行
+        sqlstring = "select location_status from location_status where updated_by = '"+ user +"' and location_at = 'home' order by updated_at desc limit 1;"
+        logger.info('sqlstring: %s', sqlstring)
+        cur.execute(sqlstring)   # rows = cur.fetchall()
+        row = cur.fetchone()
+        logger.info('row: %s', row)
+        if row[0] == 'out':
+            print("外出中")
+            return
+        else:
+            print(user, "さんは家にいる")
+
+    # 部屋の気温をDBから取得
     sqlstring = "select degree from " + checkDegree + " where updated_by='" + roomName + "' order by updated_at desc limit 1;"
     logger.info('sqlstring: %s', sqlstring)
-    cur.execute(sqlstring)
-    # rows = cur.fetchall()
+    cur.execute(sqlstring)   # rows = cur.fetchall()
     row = cur.fetchone()
     logger.info('row: %s', row)
     degree = row[0]
@@ -37,18 +54,27 @@ def lambda_handler(event, context):
     print("degree : ", degree)
     print("Month : ", nowMonth)
 
-    returnStr = ""
+    # 夏と冬で場合分け、快適な温度以外だったらjsonを作成
+    returnStr = {"season" : "", "status" : "", "roomName" : roomName, "checkDegree" : checkDegree, "degree" : degree}
     if (int(nowMonth) >= 6 and int(nowMonth) <= 10):
         if (degree > 28):
-            returnStr = {"status" : "hot", "roomName" : roomName, "checkDegree" : checkDegree, "degree" : degree}
-        if (degree < 25):
-            returnStr = {"status" : "cold", "roomName" : roomName, "checkDegree" : checkDegree, "degree" : degree}
+            returnStr = {"season" : "summer", "status" : "hot", "roomName" : roomName, "checkDegree" : checkDegree, "degree" : degree}
+            print("暑い")
+        elif (degree < 25):
+            returnStr = {"season" : "summer", "status" : "cold", "roomName" : roomName, "checkDegree" : checkDegree, "degree" : degree}
+            print("寒い")
+        else:
+            print("快適")
 
     if (int(nowMonth) >= 11 and int(nowMonth) <= 5):
         if (degree > 22):
-            returnStr = {"status" : "hot", "roomName" : roomName, "checkDegree" : checkDegree, "degree" : degree}
-        if (degree < 18):
-            returnStr = {"status" : "cold", "roomName" : roomName, "checkDegree" : checkDegree, "degree" : degree}
+            returnStr = {"season" : "winter", "status" : "hot", "roomName" : roomName, "checkDegree" : checkDegree, "degree" : degree}
+            print("暑い")
+        elif (degree < 18):
+            returnStr = {"season" : "winter", "status" : "cold", "roomName" : roomName, "checkDegree" : checkDegree, "degree" : degree}
+            print("寒い")
+        else:
+            print("快適")
 
     cur.close()
     conn.close()
